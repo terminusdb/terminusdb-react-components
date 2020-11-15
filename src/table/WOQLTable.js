32f52c079ea876151074bfbf8e90901a3b6d4513
Kevin Feeney <kevin@datachemist.com>
2020-11-15 17:12:46 +0000

    added cell rendering

--- src/table/WOQLTable.js
+++ src/table/WOQLTable.js
@@ -3,20 +3,19 @@ import TerminusClient from '@terminusdb/terminusdb-client';
 import {TableComponent} from './TableComponent';
 import { CellRenderer } from "./CellRenderer"
 
-export const WOQLTable = ({bindings, result, view, freewidth, query, start, limit, orderBy, totalRows, setLimits, setOrder, prefixes})=>{
-    //console.log(prefixes, query['@context'])
-    prefixes = prefixes || (query && query['@context'] ? query['@context'] : {})
+export const WOQLTable = ({bindings, result, view, freewidth, query, start, limit, orderBy, totalRows, setLimits, setOrder})=>{
     let wt = TerminusClient.View.table()
     if(view && view.rules)  wt.loadJSON(view.table, view.rules)
     let woqt = new TerminusClient.WOQLTable(false, wt)
     let pagenum = (limit ? parseInt((start) / limit) : 1)
-    let pages = (limit ? parseInt(((totalRows)/limit)+1) : 1)  
+    let pages = (limit ? parseInt(((totalRows-1)/limit)+1) : 1)  
+    if(totalRows == 0) pages = 0
+    let prefixes = (result && result.prefixes ? result.prefixes : (query ? query.getContext() : {}))
     
     const [data, columns]  = useMemo(() => makeData(), [bindings, result])
 
     function makeData(){
         let qres = result || {bindings: bindings}
-        view = view || {}
         let wr = new TerminusClient.WOQLResult(qres, query)
         woqt.setResult(wr, query)
         const columns = formatTableColumns(woqt)
@@ -38,7 +37,8 @@ export const WOQLTable = ({bindings, result, view, freewidth, query, start, limi
         }
         else {
             let rendargs = woqt.getRenderer(props.cell.column.id, props.cell.row.original)
-            if(typeof props.cell.value == "string" || Array.isArray(props.cell.value) || 
+            if(typeof props.cell.value == "string" || typeof props.cell.value == "number" || 
+                Array.isArray(props.cell.value) || 
                 (typeof props.cell.value == "object" && (props.cell.value['@type'] || 
                 typeof props.cell.value['@value'] != "undefined"))){
                     return <CellRenderer 
@@ -48,12 +48,12 @@ export const WOQLTable = ({bindings, result, view, freewidth, query, start, limi
                         row={props.cell.row.original} 
                         view={woqt} 
                         cell={props.cell}
+                        prefixes={prefixes}
                     />                 
             }
             return props.cell.value
         }
-    }
-    
+    }    
 
     function formatTableColumns(){
         let colids = woqt.getColumnsToRender()
@@ -62,11 +62,12 @@ export const WOQLTable = ({bindings, result, view, freewidth, query, start, limi
                 Header: woqt.getColumnHeaderContents(item) || item,
                 id: item,
                 accessor: item,
-                selector: item,
-                canSort: woqt.isSortableColumn(item),
                 Cell: function(props){
                     return renderCellValue(props, woqt)
                 }
+            }
+            if(!woqt.isSortableColumn(item)){
+                col.disableSortBy = true
             }
             if(freewidth) return col
             return addColumnDimensions(item, col, woqt)
@@ -76,7 +77,6 @@ export const WOQLTable = ({bindings, result, view, freewidth, query, start, limi
         else colstruct.Header = " "
         return [colstruct]
     }
-
     
     return(
         <TableComponent 
