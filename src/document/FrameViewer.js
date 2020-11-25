2e57005b4c57657e73eaf3fc54a88c127600deaf
Kevin Feeney <kevin@datachemist.com>
2020-11-25 11:59:10 +0000

    making document editing work

--- src/document/FrameViewer.js
+++ src/document/FrameViewer.js
@@ -3,22 +3,39 @@ import TerminusClient from '@terminusdb/terminusdb-client'
 import {TableRenderer} from "./TableRenderer"
 import {FancyRenderer} from "./FancyRenderer"
 
-export const FrameViewer = ({doc, classframe, view, client}) => {
-    if(!(classframe && classframe['system:properties'])) return null
-    let df = view.create(client)
-    df.load(classframe['system:properties'], doc)
-    df.filterFrame(getRenderer)
-    df.document.sortProperties()
-    return df.document.render() 
+export function FrameViewer(classframe, doc, view, edit, client){
+    this.frame = view.create(client)
+    this.mode = edit ? "edit" : "view"
+    if((classframe && classframe['system:properties'])) {
+        if(doc){
+            this.frame.load(classframe['system:properties'], doc)
+        }
+        else {
+            this.frame.loadSchemaFrames(classframe['system:properties'])
+        }    
+    }
+    else {
+        alert("No frames")
+        console.log(classframe)
+    }
+    const getRenderer = (name, frame, args) => {
+        const f = () => {
+            if(name == "fancy"){
+                return FancyRenderer(frame, this.mode, view)
+            }
+            return TableRenderer(frame, this.mode, view)
+        }
+        return f
+    }    
+    this.frame.filterFrame(getRenderer)
+    return this
 }
 
-const getRenderer = (name, frame, args) => {
-    const f = () => {
-        if(name == "fancy"){
-            return FancyRenderer(frame, args)
-        }
-        return TableRenderer(frame, args)
-    }
-    return f
+FrameViewer.prototype.render = function(){
+    return this.frame.document.render() 
+}
+
+FrameViewer.prototype.extract = function(){
+    return this.frame.document.extract() 
 }
 
