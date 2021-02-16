676eafe1d02394a830177041e23edf96edc15e33
Robin de Rooij <rderooij685@gmail.com>
2021-02-16 15:29:12 +0100

    ci: fix project name

--- .ci/publish_gcloud.sh
+++ .ci/publish_gcloud.sh
@@ -1,2 +1,2 @@
 BRANCH=$1
-gsutil cp -r dist/* "gs://cdn.terminusdb.com/js_libs/terminusdb_console/${BRANCH}/"
+gsutil cp -r dist/* "gs://cdn.terminusdb.com/js_libs/terminusdb-react-components/${BRANCH}/"
